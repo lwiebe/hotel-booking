fd97af8a371b8525584456483189a59a4ec13d0c
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-05-27 17:56:33 +0200

    project completed

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import Title from '../../components/Title'
 import { assets } from '../../assets/assets'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const AddRoom = () => {
+
+  const {axios, getToken} = useAppContext()
 
   const [images, setImages] = useState({
     1: null,
@@ -22,8 +26,57 @@ const AddRoom = () => {
     }
   })
 
+  const [loading, setLoading] = useState(false)
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault();
+    // Check if all inputs are filled
+    if(!inputs.roomType || !inputs.pricePerNight || !inputs.amenities || !Object.values(images).some(image => image)){
+      toast.error("Please fill in all the details")
+      return;
+    }
+    setLoading(true);
+    try {
+      const formData = new FormData()
+      formData.append('roomType', inputs.roomType)
+      formData.append('pricePerNight', inputs.pricePerNight)
+      // Converting amenities to Array & keeping only enabled Amenities
+      const amenities = Object.keys(inputs.amenities).filter(key => inputs.amenities[key])
+      formData.append('amenities', JSON.stringify(amenities))
+
+      // Adding Images to FormData
+      Object.keys(images).forEach((key)=>{
+        images[key] && formData.append('images', images[key])
+      })
+
+      const { data } = await axios.post('/api/rooms/', formData, {headers: {Authorization: `Bearer ${await getToken()}`}})
+
+      if(data.success){
+        toast.success(data.message)
+        setInputs({
+          roomType: '',
+          pricePerNight: 0,
+          amenities: {
+            'Free WiFi': false,
+            'Free Breakfast': false,
+            'Room Service': false,
+            'Mountain View': false,
+            'Pool Access': false,
+          }
+        })
+        setImages({1: null, 2: null, 3: null, 4: null})
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }finally{
+      setLoading(false);
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <Title align='left' font='outfit' title='Add Room' subTitle='Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience' />
       {/* Upload Area For Images */}
       <p className='text-gray-800 mt-10'>Images</p>
@@ -43,7 +96,7 @@ const AddRoom = () => {
           <select value={inputs.roomType} onChange={e=> setInputs({...inputs, roomType: e.target.value})}
           className='border opacity-70 border-gray-300 mt-1 rounded p-2 w-full'>
             <option value="">Select Room Type</option>
-            <option value="Sigle Bed">Sigle Bed</option>
+            <option value="Single Bed">Single Bed</option>
             <option value="Double Bed">Double Bed</option>
             <option value="Luxury Room">Luxury Room</option>
             <option value="Family Room">Family Room</option>
@@ -66,8 +119,8 @@ const AddRoom = () => {
           </div>
         ))}
       </div>
-        <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer'>
-          Add Room
+        <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer' disabled={loading}>
+          {loading ? 'Adding...' : "Add Room"}
         </button>
     </form>
   )
--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import { Webhook } from "svix";
 
-const clerkWebhooks = async (req, res)=>{
+const clerkWebhooks = async (req, res) => {
     try {
         // Create a Svix instance with clerk webhook secret.
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
@@ -19,38 +19,45 @@ const clerkWebhooks = async (req, res)=>{
         // Getting Data from request body
         const { data, type } = req.body
 
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + data.last_name,
-            image: data.image_url,
-        }
+
 
         // Switch Cases for different Events
         switch (type) {
-            case "user.created":{
+            case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
                 await User.create(userData);
                 break;
             }
 
-            case "user.updated":{
+            case "user.updated": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
                 await User.findByIdAndUpdate(data.id, userData);
                 break;
             }
 
-            case "user.deleted":{
+            case "user.deleted": {
                 await User.findByIdAndDelete(data.id);
                 break;
             }
-        
+
             default:
                 break;
         }
-        res.json({success: true, message: "Webhook Received"})
+        res.json({ success: true, message: "Webhook Received" })
 
     } catch (error) {
         console.log(error.message);
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
